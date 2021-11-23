50e486b23b8c19a1cbbd0ca58b2fd76eb542e6cf
Kamil Plich <kamilplich2@gmail.com>
2021-11-23 23:42:09 +0100

    preserve 'main' methods

--- Obfuscation/Core/RandomMethodRenamer.cs
+++ Obfuscation/Core/RandomMethodRenamer.cs
@@ -14,16 +14,32 @@ namespace Obfuscation.Core
             var methodDeclarations = (await syntaxTree.GetRootAsync()).DescendantNodes().OfType<MethodDeclarationSyntax>();
             var methodSymbols
                 = methodDeclarations
-                    .Select(methodDeclaration => semanticModel.GetDeclaredSymbol(methodDeclaration)!)
-                    .Where(symbol => symbol != null);
+                    .Select(methodDeclaration => semanticModel.GetDeclaredSymbol(methodDeclaration))
+                    .Where(symbol => symbol != null)
+                    .OfType<IMethodSymbol>();
             
             foreach (var methodSymbol in methodSymbols)
             {
-                solution = await Renamer.RenameSymbolAsync(solution, methodSymbol,
-                    Guid.NewGuid().ToString().Replace("-", "").Insert(0, "_"), solution.Workspace.Options);
+                if (methodSymbol.IsNotMain())
+                {
+                    solution = await Renamer.RenameSymbolAsync(solution, methodSymbol,
+                        Guid.NewGuid().ToString().Replace("-", "").Insert(0, "_"), solution.Workspace.Options);
+                }
             }
 
             return solution;
         }
+    }
+
+    internal static class RandomMethodRenamerUtils
+    {
+        private static bool IsMain(this IMethodSymbol methodSymbol) =>
+            methodSymbol.IsStatic &&
+            methodSymbol.Name == "Main" &&
+            methodSymbol.ReturnsVoid &&
+            methodSymbol.Arity == 0 &&
+            methodSymbol.DeclaredAccessibility == Microsoft.CodeAnalysis.Accessibility.Private;
+
+        internal static bool IsNotMain(this IMethodSymbol methodSymbol) => !methodSymbol.IsMain();
     }
 }
