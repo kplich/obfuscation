34e22cfd3ae16e3336e7e8231e4b72a3e932af87
Kamil Plich <kamilplich2@gmail.com>
2022-01-16 00:53:47 +0100

    remove analysis view

--- Obfuscation/MainViewModel.cs
+++ Obfuscation/MainViewModel.cs
@@ -1,20 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using Obfuscation.Controls.Analysis;
-using Obfuscation.Controls.Obfuscation;
-using Obfuscation.Core;
-
-namespace Obfuscation
+﻿namespace Obfuscation
 {
     public class MainViewModel
     {
-        public ICollection<TabViewModel> Tabs { get; }
-
-        public MainViewModel()
-        {
-            Tabs = new ObservableCollection<TabViewModel>();
-            Tabs.Add(new ObfuscationViewModel());
-            Tabs.Add(new AnalysisViewModel());
-        }
     }
 }
